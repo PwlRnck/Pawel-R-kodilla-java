d0ccb23111fe9b910dce702ece818aa47ae0c7b0
“PwlRnck <p.reniecki@gmail.com>
2017-07-15 00:35:01 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 /**
  * Created by PR on 12.07.2017.
  */
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("Class: SimpleUser; method: getUsername; test OK");
+        } else {
+            System.out.println("Class: SimpleUser; method: getUsername; Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        if(calculator.add(2,2)==2+2){
+            System.out.println("Class: calculator; method: add; test OK");
+        } else {
+            System.out.println("Class: calculator; method: add; Error!");
+        }
+
+        if(calculator.subtract(5,2)==5-2){
+            System.out.println("Class: calculator; method: subtract; test OK");
+        } else {
+            System.out.println("Class: calculator; method: subtract; Error!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.calculator;
+
+/**
+ * Created by PR on 15.07.2017.
+ */
+public class Calculator {
+
+    public int add(int a, int b){
+            return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+}
