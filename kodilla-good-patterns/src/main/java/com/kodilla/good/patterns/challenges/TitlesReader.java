18728b9cce8cfe2699b1239a81a46b5e8f322c0f
“PwlRnck <p.reniecki@gmail.com>
2017-08-16 09:27:42 +0200

    good-patterns: titles reader, corrected formatting

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesReader.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesReader.java
@@ -7,7 +7,8 @@ public class TitlesReader {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        String titles = movieStore.getMovies().entrySet().stream().flatMap(entry -> entry.getValue().stream())
+        String titles = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
                 .reduce("",(conc,nextStr) -> conc = conc + "!" + nextStr);
 
         System.out.println(titles.substring(1));
