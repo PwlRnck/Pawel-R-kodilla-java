56445c118eb1e360cc28a4782d76e79cea5be5ea
“PwlRnck <p.reniecki@gmail.com>
2017-08-28 14:54:48 +0200

    spring: portfolio

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.portfolio;
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //When
+        board.getToDoList().addTask("toDoTask: task1");
+        board.getInProgressList().addTask("inProgressTask: task2");
+        board.getDoneList().addTask("doneTask: task3");
+        //Then
+        board.getToDoList().getTasks().stream().forEach(System.out::println);
+        board.getInProgressList().getTasks().stream().forEach(System.out::println);
+        board.getDoneList().getTasks().stream().forEach(System.out::println);
+    }
+}
