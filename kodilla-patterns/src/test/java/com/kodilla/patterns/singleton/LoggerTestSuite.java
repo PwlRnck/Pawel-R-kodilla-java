4d772bf61c1151d4f42d845e77747578b0339eac
“PwlRnck <p.reniecki@gmail.com>
2017-09-03 00:23:08 +0200

    patterns: logger junit of System.out

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -3,15 +3,25 @@ package com.kodilla.patterns.singleton;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class LoggerTestSuite {
     @Test
     public void testLog() {
         //Given
         Logger logger = Logger.getInstance();
         //When
+        PrintStream originalOut = System.out;
+        OutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        System.setOut(ps);
         logger.log("test1");
+        String output = os.toString();
+        System.setOut(originalOut);
         //Then
-
+        Assert.assertEquals("Log: [test1]" +  System.getProperty("line.separator"),output);
     }
 
     @Test
