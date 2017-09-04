f7c7ba82759f26f7f7600c6c51c4c9c7e0490c78
“PwlRnck <p.reniecki@gmail.com>
2017-09-04 16:00:28 +0200

    patterns: library improved

--- kodilla-patterns/src/test/java/com/kodilla/patterns/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/library/LibraryTestSuite.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 
 public class LibraryTestSuite {
     @Test
-    public void testGetBooks() {
+    public void testGetBooksShallowCopy() {
         //Given
         Library library = new Library("Central Library");
         IntStream.iterate(1,n -> n+1)
@@ -23,30 +23,42 @@ public class LibraryTestSuite {
             System.out.println(e);
         }
 
-        Library deepCopy = null;
-        try {
-            deepCopy = library.deepCopy();
-            shallowCopy.setName("Affiliate Library 2");
-        } catch (CloneNotSupportedException e) {
-            System.out.println(e);
-        }
-
-
         //When
-        //shallowCopy.getBooks().add(new Book("New Book","New Author",LocalDate.parse("2017-01-01")));
-        shallowCopy.books.add(new Book("New Book","New Author",LocalDate.parse("2017-01-01")));
-        deepCopy.getBooks().remove(4);
-
-
+        shallowCopy.getBooks().add(new Book("New Book","New Author",LocalDate.parse("2017-01-01")));
         int result1 = library.getBooks().size();
         int result2 = shallowCopy.getBooks().size();
-        int result3 = deepCopy.getBooks().size();
 
         //Then
         Assert.assertEquals(6,result1);
         Assert.assertEquals(6,result2);
-        Assert.assertEquals(5,result3);
         Assert.assertEquals(library.getBooks(),shallowCopy.getBooks());
+
+    }
+
+    @Test
+    public void testGetBooksDeepCopy() {
+        //Given
+        Library library = new Library("Central Library");
+        IntStream.iterate(1,n -> n+1)
+                .limit(5)
+                .forEach(n -> library.getBooks().add(new Book("Book#" + n,"Author#" + n,LocalDate.parse("2000-01-01"))));
+
+        Library deepCopy = null;
+        try {
+            deepCopy = library.deepCopy();
+            deepCopy.setName("Affiliate Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        deepCopy.getBooks().add(new Book("New Book","New Author",LocalDate.parse("2017-01-01")));
+        int result1 = library.getBooks().size();
+        int result3 = deepCopy.getBooks().size();
+
+        //Then
+        Assert.assertEquals(5,result1);
+        Assert.assertEquals(6,result3);
         Assert.assertNotEquals(library.getBooks(),deepCopy.getBooks());
     }
 }
